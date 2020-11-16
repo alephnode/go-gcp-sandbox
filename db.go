dc80ed24601e6ac9702733e1795dcf1f2adc54bf
srmward <srmward@gmail.com>
2020-11-15 22:48:05 -0800

    Clean Up Code

--- db.go
+++ db.go
@@ -10,7 +10,7 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-func checkDbIntegration() {
+func getAuthorName() (string, error) {
 
 	var (
 		dbUser                 = os.Getenv("DB_USER")
@@ -28,36 +28,28 @@ func checkDbIntegration() {
 
 	dbURI = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, dbPwd, dbName, socketDir, instanceConnectionName)
 
-	dbPool, dbError := sql.Open("pgx", dbURI)
+	dbPool, err := sql.Open("pgx", dbURI)
 
-	if dbError != nil {
-		log.Fatalf("%s", dbError)
+	if err != nil {
+		log.Fatalf("%s", err)
 	}
 
-	// defer dbPool.Close()
-
-	erryore := dbPool.Ping()
-	if erryore != nil {
-		log.Println("ERROR -- could not establish connection")
-		panic(erryore)
-	}
-
-	sqlStatement := `SELECT author_name FROM authors WHERE author_id=$1;`
-	authorID := 1
 	var authorName string
+	authorID := 3
+	sqlStatement := `SELECT author_name FROM authors WHERE author_id=$1;`
 
 	row := dbPool.QueryRow(sqlStatement, authorID)
 
-	switch errp := row.Scan(&authorName); errp {
+	switch err = row.Scan(&authorName); err {
 	case sql.ErrNoRows:
 		log.Println("ERROR -- No rows were returned!")
 	case nil:
 		log.Println("INFO -- connected to authors table")
 		log.Printf("author name is %q", authorName)
 	default:
-		log.Println("ERROR -- I panic, tbh")
-		log.Printf("ERROR -- %q", errp)
-		panic(errp)
+		log.Printf("ERROR -- %q", err)
+		panic(err)
 	}
 
+	return authorName, err
 }
--- get_http.go
+++ get_http.go
@@ -1,27 +1,18 @@
 package gethttp
 
 import (
-	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
 // GetHTTP is the cloud fn
 func GetHTTP(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Name string `json:"name"`
+	name, err := getAuthorName()
+
+	if err != nil {
+		log.Printf("ERROR -- %v", err)
 	}
-	checkDbIntegration()
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello, World!")
-		return
-	}
-	if d.Name == "" {
-		fmt.Fprint(w, "Hello, World!")
-		return
-	}
-	log.Println("I have been called.")
-	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+
+	fmt.Fprintf(w, "Hello, %s!", name)
 }
